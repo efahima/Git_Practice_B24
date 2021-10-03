46341c9eb3f00164e8b1c84c2d5545bf9082f868
Fahima <efahima76@gmail.com>
2021-10-03 12:45:58 -0400

    Created third file

---
+++ src/setup/Third.java
@@ -0,0 +1,17 @@
+package setup;
+
+public class Third {
+        public void m(){
+            System.out.println("Third Method");
+            System.out.println("I am practicing my Git");
+
+        }
+        public void m(int a){
+
+        }
+        public void m(String s){
+
+        }
+    }
+
+
